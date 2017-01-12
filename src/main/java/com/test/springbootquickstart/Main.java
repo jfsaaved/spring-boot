c6b5f575e6db9b877094db110f79022e22b7c532
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2017-01-12 11:53:44 -0500

    January 12, 2017 - Commit 1 - I would like to thank god for helping me solve this problem

--- src/main/java/com/test/springbootquickstart/Main.java
+++ src/main/java/com/test/springbootquickstart/Main.java
@@ -1,5 +1,4 @@
-package com.jfsaaved.springbootstarter;
-
+package com.test.springbootquickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
